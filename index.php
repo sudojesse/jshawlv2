7ad85af0e6c609fca18d4181b3d7ea94ac58613d
Jesse Shawl <jesseshawl@gmail.com>
2013-01-26 10:05:31 -0500

    boutta get crazy

--- index.php
+++ index.php
@@ -11,7 +11,8 @@
         <meta name="viewport" content="width=device-width">
 
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-
+        <script type="text/javascript" src="//use.typekit.net/nwv6kix.js"></script>
+        <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
         <link rel="stylesheet" href="styles.css">
         <script src="js/vendor/modernizr-2.6.2.min.js"></script>
     </head>
@@ -20,8 +21,43 @@
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
 
-        <!-- Add your site or application content here -->
-        <p>Hello world! This is HTML5 Boilerplate.</p>
+       
+
+        <div class='first'>
+            <header>
+                <h1>Jesse Shawl</h1>
+            </header>
+            <span class='circles'>
+                <span class='circle--1 light'>
+                    <span>is a front end</span>
+                    <span class='smiley'>:)</span>
+                    <span class='upper boldest'>web developer</span>
+                </span>
+                <span class='circle--2 light'>
+                    <span>&amp; web development</span>
+                    <span class='upper boldest'>consultant</span>
+                </span>
+            </span> <!-- ends .circles -->
+        </div><!-- ends first -->
+
+        <div class='second'>
+            <div id='heart'><p>Loves the web</p></div>
+        </div>
+
+         <div class='third' id='three'>
+            <h2>Recent Achievements:</h2>
+            <ul>
+                <li><a href='#'>1/27 Launched christopherbaer.com</a></li>
+                <li><a href='#'>Article on HTML5 history API posted on CSS-Tricks</a></li>
+                <li><a href='#'>Launched speedycarrental.net</a></li>
+            </ul>
+        </div>
+
+        <?php 
+            echo '<pre>';
+            print_r($_REQUEST);
+        ?>
+    
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
+$('header h1').animate({
+	'top':0,
+	'opacity':1
+}, 700, function(){
+	$('header h2 span').lettering();
+	$('header h2 span').show();
+	$('header h2 span').children().each(function(index){
+		$(this).delay(30*index).animate({
+			'opacity':1,
+			'top':'-1px'
+		},150);
+	});
+});
 
+;(function($){
+	$(window).scroll(function(){
+		pos = $(this).scrollTop();
+		$('.circle--2').css({
+			'left':pos+'px',
+			'opacity': (160 - (pos/2))/100,
+			'-webkit-transform':'rotate('+pos+'deg)',
+			'-moz-transform':'rotate('+pos+'deg)',
+		});
+		$('.circle--1').css({
+			'right':pos+'px',
+			'opacity': (160 - (pos/2))/100,
+			'-webkit-transform':'rotate('+(pos*-1)+'deg)',
+			'-moz-transform':'rotate('+(pos*-1)+'deg)'
+		});
+	});
+})(jQuery);
